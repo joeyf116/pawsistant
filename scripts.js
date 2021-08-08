6db2c28b0e30a8e3fb30a158ab60d265c77f3b3d
joeyf116 <jfitzdevelop@yahoo.com>
2021-08-08 13:48:03 -0400

    Services tab with animations

--- scripts.js
+++ scripts.js
@@ -2,8 +2,50 @@
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
-  
+
 // Close Nav Overlay
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
+}
+
+function seeMore() {
+  if(document.getElementById("services-list-see-more-tag").innerHTML === "See more!") {
+    document.getElementById("services-list").style.display = "flex";
+    document.getElementById("services-list").style.flexDirection = "column";
+    document.getElementById("services-list-see-more-tag").innerHTML = "See Less"
+    document.getElementById("services-list").animate([
+      // keyframes
+      { transform: 'translateY(100px)' },
+      { transform: 'translateY(0px)' }
+    ], {
+      // timing options
+      duration: 1000,
+      iterations: 1
+    });
+  }
+  else {
+    document.getElementById("services-list").style.display = "none";
+    document.getElementById("services-list-see-more-tag").innerHTML = "See more!"
+  }
+}
+
+function seeMore2() {
+  if(document.getElementById("services-list-see-more-tag2").innerHTML === "See more!") {
+    document.getElementById("services-list2").style.display = "flex";
+    document.getElementById("services-list2").style.flexDirection = "column";
+    document.getElementById("services-list-see-more-tag2").innerHTML = "See Less"
+    document.getElementById("services-list2").animate([
+      // keyframes
+      { transform: 'translateY(100px)' },
+      { transform: 'translateY(0px)' }
+    ], {
+      // timing options
+      duration: 1000,
+      iterations: 1
+    });
+  }
+  else {
+    document.getElementById("services-list2").style.display = "none";
+    document.getElementById("services-list-see-more-tag2").innerHTML = "See more!"
+  }
 }
