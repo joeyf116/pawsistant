ddee509830555b7808b0b925e345dc182cc708af
joeyf116 <jfitzdevelop@yahoo.com>
2021-08-07 21:35:23 -0400

    Add comments to scripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
+// Open Nav Overlay
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
   
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+// Close Nav Overlay
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
